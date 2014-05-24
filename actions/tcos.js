b5a1d8e4437f155c8ceaf2c1136342a16d03f726
BharathMG <mgbharath@ymail.com>
2014-05-25 02:20:02 +0530

    Implemented #2 GET /tcos/#{tco_id}/

--- actions/tcos.js
+++ actions/tcos.js
@@ -1,4 +1,4 @@
-exports.action = {
+exports.tcosList = {
   name:                   'tcosList',
   description:            'Returns all tco records. Method: GET',
   outputExample:          {},
@@ -12,6 +12,27 @@ exports.action = {
 
   run: function(api, connection, next){
     api.tcos.list(function(data){
+      connection.response.response = data;
+      connection.response.count = data.length;
+      next(connection, true);
+    });
+  }
+};
+
+exports.tco = {
+  name:                   'tco',
+  description:            'Returns a single tco record. Method: GET',
+  outputExample:          {},
+  matchExtensionMimeType: false,
+  version:                1.0,
+  toDocument:             true,
+  inputs: {
+    required: ['id'],
+    optional: [],
+  },
+
+  run: function(api, connection, next){
+    api.tcos.get(connection.params,function(data){
       connection.response.response = data;
       connection.response.count = data.length;
       next(connection, true);
--- routes.js
+++ routes.js
@@ -45,6 +45,7 @@ exports.routes = {
   get: [
     { path: '/tcos/:tco_id/events/:id', action: 'event'},
     { path: '/tcos/:tco_id/events', action: 'eventsList' },
+    { path: '/tcos/:id', action: 'tco' },
     { path: '/tcos', action: 'tcosList' }
   ]
 
