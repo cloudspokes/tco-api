0bdb66997fdf7fe97aad538dd9a03881c18a3923
Mohamed Hammad <ibraaaa@gmail.com>
2014-05-26 12:33:24 +0200

    Fixes #36 - Move albums to separate files

--- actions/tcos.js
+++ actions/tcos.js
@@ -18,26 +18,3 @@ exports.action = {
     });
   }
 };
-
-exports.album = {
-  name:                   'album',
-  description:            'Returns a single TCO Album. Method: GET',
-  outputExample:          {},
-  matchExtensionMimeType: false,
-  version:                1.0,
-  toDocument:             true,
-  inputs: {
-    required: [ 'tco_id', 'id' ],
-    optional: [],
-  },
-
-  run: function(api, connection, next){
-    api.tcos.album(connection.params.tco_id, connection.params.id,
-      function(data){
-        connection.response.response = data;
-        connection.response.count = data.length;
-        next(connection, true);
-      }
-    );
-  }
-};
---
+++ test/albums.js
@@ -0,0 +1,39 @@
+var request = require("request");
+var should = require('chai').should();
+var setup = require("./_setup.js")._setup;
+
+describe('albums', function(){
+
+  before(function(done){
+    setup.init(done);
+  });
+
+  after(function(done){
+    // no teadrown needed at this time
+    done();
+  });
+
+  /*
+   * Tests Retrieve Album: GET /tcos/{tco_id}/albums/{id}
+   */
+  it("/tcos/{tco_id}/albums/{id} should return an album", function(done) {
+    this.timeout(5000);
+    attributes = [ 'id', 'tco_id', 'name', 'cover' ];
+    tco_id = 'tco14';
+    id = 1;
+    request.get(setup.testUrl + "/tcos/" + tco_id + "/albums/" + id,
+      function (err, res, body) {
+        body = JSON.parse(body);
+        console.log(body);
+        res.statusCode.should.equal(200);
+        body.count.should.equal(1);
+        body.response.should.be.an.instanceof(Array);
+        body.response[0].tco_id.should.equal(tco_id);
+        body.response[0].id.should.equal(id);
+        body.response[0].should.have.keys(attributes);
+        done();
+      }
+    );
+  });
+
+});
