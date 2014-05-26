bd5017aa910a5f00c3b14fbf5cfede9cef2da9c8
Mohamed Hammad <ibraaaa@gmail.com>
2014-05-26 13:34:48 +0200

    Fixes #30 - REDO: Retrieve News: GET /tcos/{tco_id}/news/{id}

---
+++ actions/news.js
@@ -0,0 +1,22 @@
+exports.news = {
+  name:                   'news',
+  description:            'Returns a single News object. Method: GET',
+  outputExample:          {},
+  matchExtensionMimeType: false,
+  version:                1.0,
+  toDocument:             true,
+  inputs: {
+    required: [ 'tco_id', 'id' ],
+    optional: [],
+  },
+
+  run: function(api, connection, next){
+    api.news.get(connection.params.tco_id, connection.params.id,
+      function(data){
+        connection.response.response = data;
+        connection.response.count = data.length;
+        next(connection, true);
+      }
+    );
+  }
+};
---
+++ test/news.js
@@ -0,0 +1,40 @@
+var request = require("request");
+var should = require('chai').should();
+var setup = require("./_setup.js")._setup;
+
+describe('tcos', function(){
+
+  before(function(done){
+    setup.init(done);
+  });
+
+  after(function(done){
+    // no teadrown needed at this time
+    done();
+  });
+
+  /*
+   * Tests Retrieve News: GET /tcos/{tco_id}/news/{id}
+   */
+  it("/tcos/{tco_id}/news/{id} should return a news object", function(done) {
+    this.timeout(5000);
+    attributes = [ 'id', 'tco_id', 'name', 'source_url',
+                   'source', 'creation_date', 'content' ];
+    tco_id = 'tco14';
+    id = 1;
+    request.get(setup.testUrl + "/tcos/" + tco_id + "/news/" + id,
+      function (err, res, body) {
+        body = JSON.parse(body);
+        console.log(body);
+        res.statusCode.should.equal(200);
+        body.count.should.equal(1);
+        body.response.should.be.an.instanceof(Array);
+        body.response[0].tco_id.should.equal(tco_id);
+        body.response[0].id.should.equal(id);
+        body.response[0].should.to.have.keys(attributes);
+        done();
+      }
+    );
+  });
+
+});
