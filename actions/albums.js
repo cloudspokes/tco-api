9f2bb948f5053452e9d2a91c23498ae4627afa21
Mohamed Hammad <ibraaaa@gmail.com>
2014-05-26 13:14:10 +0200

    Fixes #36 - REDO: Get /tcos/{tco_id}/albums/{id}

---
+++ actions/albums.js
@@ -0,0 +1,22 @@
+exports.album = {
+  name:                   'album',
+  description:            'Returns a single TCO Album. Method: GET',
+  outputExample:          {},
+  matchExtensionMimeType: false,
+  version:                1.0,
+  toDocument:             true,
+  inputs: {
+    required: [ 'tco_id', 'id' ],
+    optional: [],
+  },
+
+  run: function(api, connection, next){
+    api.albums.get(connection.params.tco_id, connection.params.id,
+      function(data){
+        connection.response.response = data;
+        connection.response.count = data.length;
+        next(connection, true);
+      }
+    );
+  }
+};
