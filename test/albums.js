968d4dd14d51d118bb88942d3af09c4229810b1a
Mohamed Hammad <ibraaaa@gmail.com>
2014-05-28 16:33:42 +0200

    Fix #37 - POST /tcos/{tco_id}/albums/{id}/like

--- test/albums.js
+++ test/albums.js
@@ -1,6 +1,8 @@
 var request = require("request");
 var should = require('chai').should();
 var setup = require("./_setup.js")._setup;
+var pg = require('pg').native;
+var Q = require("q");
 
 describe('albums', function(){
 
@@ -94,5 +96,82 @@ describe('albums', function(){
       }
     );
   });
+
+  /*
+   * Tests liking an album: POST /tcos/{tco_id}/albums/{id}/like
+   */
+  it("/tcos/{tco_id}/albums/{id}/like should like an album.", function(done) {
+    this.timeout(25000);
+    postLike()
+      .then(function() {
+        return checkLiked();
+      })
+      .then(function() {
+        var client = new pg.Client(setup.api.config.general.pg.connString);
+        return createConnectionAsPromised(client);
+      })
+      .then(function(client) {
+        return executeCleanupQueryAsPromised(client);
+      })
+      .then(function(rs) {
+        rs.rowCount.should.equal(1);
+        done();
+      })
+      .fail(function(err) {
+        console.log(err);
+        should.not.exist(err);
+      });
+
+  });
+
+  function createConnectionAsPromised(client) {
+    var deferred = Q.defer();
+    client.connect(function(err) {
+      if (err) deferred.reject("Error: no connection to postgres database");
+      if (!err) deferred.resolve(client);
+    });
+    return deferred.promise;
+  }
+
+  function executeCleanupQueryAsPromised(client) {
+    var deferred = Q.defer();
+    var sql = "DELETE FROM salesforce.tco_favorite__c WHERE fav_album__c" +
+    " IN (SELECT a.sfid FROM salesforce.tco_album__c as a WHERE a.id = 2)";
+    client.query(sql, function(err, rs) {
+      if (err) deferred.reject(err);
+      if (!err) deferred.resolve(rs);
+    });
+    return deferred.promise;
+  }
+
+  function checkLiked(albumId) {
+    var deferred = Q.defer();
+    request.get(setup.testUrl + "/tcos/tco14/albums/2/like",
+      function (err, res, body) {
+        body = JSON.parse(body);
+        res.statusCode.should.equal(200);
+        should.equal(body.count, null);
+        body.response.should.be.an.instanceof(Object);
+        body.response.liked.should.be.true;
+        deferred.resolve();
+      }
+    );
+
+    return deferred.promise;
+  }
+
+  function postLike() {
+    var deferred = Q.defer();
+    request.post(setup.testUrl + "/tcos/tco14/albums/2/like",
+      function (err, res, body) {
+        body = JSON.parse(body);
+        res.statusCode.should.equal(200);
+        body.response.rowCount.should.equal(1);
+        deferred.resolve();
+      }
+    );
+
+    return deferred.promise;
+  }
 
 });
