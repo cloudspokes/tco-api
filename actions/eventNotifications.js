d453fac9123f49bc2d981dd6bbc938007cd04885
notpad <liuweixuan@liuweixuantekiMacBook-Pro.local>
2014-05-30 23:24:48 +0800

    Fixes #8 - post event notification

--- actions/eventNotifications.js
+++ actions/eventNotifications.js
@@ -39,3 +39,26 @@ exports.eventNotificationsList = {
     });
   }
 };
+
+exports.postEventNotification = {
+  name:                   'postEventNotification',
+  description:            'Post a single event notification object. Method: POST',
+  outputExample:          {},
+  matchExtensionMimeType: false,
+  version:                1.0,
+  toDocument:             true,
+  inputs: {
+    required: [ 'tco_id' , 'id'],
+    optional: []
+  },
+
+  run: function(api, connection, next){
+    api.eventNotifications.post(connection.params,
+      function(data){
+        connection.response.response = data;
+        connection.response.count = data.length;
+        next(connection, true);
+      }
+    );
+  }
+};
