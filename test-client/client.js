b435d55d6035a44a149a54b93cc262dd3e5635c5
Jeff Douglas <jeff@jeffdouglas.com>
2014-07-21 13:50:41 -0400

    test script

---
+++ test-client/client.js
@@ -0,0 +1,17 @@
+var request = require('request'); // request install -g
+
+request.post('http://localhost:8080/api/tcos/tco14/signin', {form:{email:'sample@tcosapi.com', password: 'sample'}}, function(err, response, body) {
+  if (err) console.log(err);
+  if (!err) {
+
+    // route required authorization
+    request('http://localhost:8080/api/tcos/tco14/sponsors', function (err, response, body) {
+      if (!err && response.statusCode == 200) {
+        console.log(body)
+      } else {
+        console.log(response.statusCode);
+      }
+    })
+
+  }
+});
