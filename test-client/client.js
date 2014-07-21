d34cc112fafb6fda567b9abeafd973cbedb57f00
Mohamed Hammad <ibraaaa@gmail.com>
2014-07-21 21:08:30 +0200

    Session should use the connection finger print (first part of the connection ID)

--- test-client/client.js
+++ test-client/client.js
@@ -1,5 +1,8 @@
 var request = require('request'); // request install -g
 
+// Enable cookies.
+request = request.defaults({jar: true});
+
 request.post('http://localhost:8080/api/tcos/tco14/signin', {form:{email:'sample@tcosapi.com', password: 'sample'}}, function(err, response, body) {
   if (err) console.log(err);
   if (!err) {
