f4c1ad2e0eb16d9f910f74a407d40c173e3cbe39
Jeff Douglas <jeff@jeffdouglas.com>
2014-07-30 11:32:23 -0400

    pass back user after successful signup

--- initializers/signin.js
+++ initializers/signin.js
@@ -20,7 +20,7 @@ exports.signin = function(api, next) {
       user.connection_id = connection.id;
       connection.rawConnection.req.logIn(user, function () {
 
-        // pass back their user info. -- TODO move query to attendee initializer for reuse
+        // pass back their user info. -- TODO move query to attendee initializer for reuse 
         var client = new pg.Client(api.config.general.pg.connString);
         client.connect(function(err) {
 
