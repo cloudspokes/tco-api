742127ed6910568436e2578824a6a9393c1546db
Jeff Douglas <jeff@jeffdouglas.com>
2014-08-01 09:38:48 -0400

    updated

--- test-client/client.js
+++ test-client/client.js
@@ -6,9 +6,9 @@ request = request.defaults({jar: true});
 request.post('http://localhost:8080/api/tcos/tco14/signin', {form:{email:'sample@tcosapi.com', password: 'sample'}}, function(err, response, body) {
   if (err) console.log(err);
   if (!err) {
-
+    
     // route required authorization
-    request('http://localhost:8080/api/tcos/tco14/sponsors', function (err, response, body) {
+    request('http://localhost:8080/api/tcos/tco14/news', function (err, response, body) {
       if (!err && response.statusCode == 200) {
         console.log(body)
       } else {
